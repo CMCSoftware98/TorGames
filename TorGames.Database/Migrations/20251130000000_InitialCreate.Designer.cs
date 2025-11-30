057e37c2dedecbed33c6b1cf8111478c7c3bc2eb
Callum Cross <callum.cross@immixprotect.com>
2025-11-30 23:14:25 +0000

    Updates EF Core to version 10 preview 6
    Updates Entity Framework Core and related packages to the 10.0.0-preview.6.25314.1 version.
    
    This ensures compatibility with the latest features and improvements in EF Core.

--- TorGames.Database/Migrations/20251130000000_InitialCreate.Designer.cs
+++ TorGames.Database/Migrations/20251130000000_InitialCreate.Designer.cs
@@ -18,7 +18,7 @@ namespace TorGames.Database.Migrations
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
+            modelBuilder.HasAnnotation("ProductVersion", "10.0.0-preview.6.25314.1");
 
             modelBuilder.Entity("TorGames.Database.Entities.Client", b =>
                 {
